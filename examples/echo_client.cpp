1c0116ecf1719f5b17114bc0ee48d66704c9606b
goalworld <583032753@qq.com>
2014-04-28 14:47:44 +0800

    a

--- examples/echo_client.cpp
+++ examples/echo_client.cpp
@@ -1,10 +1,41 @@
-/*
- * echo_client.hpp
- *
- *  Created on: 2014年4月27日
- *      Author: wd
- */
+#include <abb/base/log.hpp>
+#include <abb/abb.hpp>
+#include <abb/net/acceptor.hpp>
 
-
-
-
+class ConnectCB:public abb::net::Connection::IEvent{
+class IEvent{
+	public:
+		virtual void Connection_Event(int ev){
+			LOG(INFO)<< ev;
+		}
+	};
+class EventCb:public abb::net::Connector::IEvent{
+public:
+	virtual ~EventCb(){}
+	virtual void Connector_Open(Connection* conn){
+		LOG(INFO)<< "Connector_Open";
+		conn->SetEnable(true);
+		conn->SetEventCallBack(new ConnectCB());
+	}
+	virtual void Connector_OpenFail(int err){
+		LOG(INFO)<< "Connector_OpenFail" << err << strerror(err);
+	}
+};
+int main(){
+	LOG(INFO) << "init";
+	abb::Init();
+	LOG(INFO) << "c";
+	abb::net::IPAddr addr;
+	if( ! addr.SetV4("127.0.0.1",9922) ){
+		LOG(INFO) << "setv4 fail";
+		return -1;
+	}
+	abb::net::Connector* ac = abb::net::Connector::New();
+	EventCb ev;
+	ac->SetEventCallback(&ev);
+	if( !ac->Connect(addr) ){
+		LOG(INFO) << "Connect fail";
+		return -1;
+	}
+	abb::Run();
+}
