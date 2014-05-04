678895f83349ba84aad47949870170f3cfde34a0
goalworld <583032753@qq.com>
2014-05-04 17:33:07 +0800

    a

--- src/net/connection.cpp
+++ src/net/connection.cpp
@@ -112,6 +112,7 @@ void Connection::PollerEvent_OnRead(){
 	int size = this->rd_buf_.Size();
 	rd_lock_.UnLock();
 	if(size >  0 || this->err_){
+		LOG(INFO) << "Dispatch" <<size << err_;
 		if(this->err_){
 			loop_.GetPoller().DelReadWrite(&this->entry_);
 		}
