abcdab7a6d23467e6b2d5252237625bf5032d9d7
goalworld <583032753@qq.com>
2014-05-05 11:37:06 +0800

    a

--- src/net/epoll_poller.cpp
+++ src/net/epoll_poller.cpp
@@ -99,10 +99,11 @@ void Poller::Poll(int timeout){
 		if(revs[i].events &	(EPOLLOUT | EPOLLHUP | EPOLLERR)){
 			revent = POLLER_WRITE;
 		}
-		if(revs[i].events &(EPOLLHUP | EPOLLERR)){
-			LOG(INFO) << "epoll_wait" << revs[i].events;
-		}
+
 		entry = (Entry*)(revs[i].data.ptr);
+		if(revs[i].events &(EPOLLHUP | EPOLLERR)){
+					LOG(INFO) << "epoll_wait" << revs[i].events << entry->event_;
+				}
 		if(entry)
 			entry->Emitter(revent);
 	}
