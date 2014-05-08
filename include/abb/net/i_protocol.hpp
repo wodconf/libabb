26675df60f0b992ac4af9669bf511744b4a28533
goalworld <583032753@qq.com>
2014-05-08 13:50:09 +0800

    a

--- include/abb/net/i_protocol.hpp
+++ include/abb/net/i_protocol.hpp
@@ -9,15 +9,15 @@ typedef void* Msg;
 class IProtocol{
 public:
 	virtual ~IProtocol(){};
-	void Encode(base::Buffer& buf,Msg msg);
-	Msg Decode(base::Buffer& buf);
+	void Encode(base::Buffer& buf,Msg msg)=0;
+	Msg Decode(base::Buffer& buf)=0;
 };
 
 class IProtocolFactory{
 public:
 	virtual ~IProtocolFactory(){};
-	IProtocol* CreateProtocol();
-	void DestroyProtocol(IProtocol*val);
+	IProtocol* CreateProtocol()=0;
+	void DestroyProtocol(IProtocol*val)=0;
 };
 }
 }
