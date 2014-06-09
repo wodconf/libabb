5bb821a0c1de7da74e4c4c2f8cb6222b1b501919
wd <583032753@qq.com>
2014-06-09 10:04:01 +0800

    xxxxx

---
+++ include/abb/net/tcp_client.hpp
@@ -0,0 +1,26 @@
+
+#ifndef __ABB_NET_TCP_CLIENT_HPP_
+#define __ABB_NET_TCP_CLIENT_HPP_
+#include "listener.hpp"
+#include "connection_ref.hpp"
+#include "ip_addr.hpp"
+namespace abb {
+namespace net {
+
+class Loop;
+class ITcpClientListener{
+public:
+	virtual ~ITcpClientListener(){}
+	virtual void L_TcpClient_OnConnectFail(int error) = 0;
+	virtual void L_TcpClient_OnConnection(ConnectionRef* conn) = 0;
+	virtual void L_TcpClient_OnMessage(ConnectionRef* conn,base::Buffer& buf) = 0;
+	virtual void L_TcpClient_OnClose(ConnectionRef* conn,int error) = 0;
+};
+namespace tcp{
+extern bool Connect(Loop* loop,const IPAddr& addr,int* save_error,ITcpClientListener* lis);
+}
+
+} /* namespace monprxoy */
+} /* namespace adcloud */
+
+#endif /* TCP_CLIENT_HPP_ */
