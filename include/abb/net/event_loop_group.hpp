cb2d489c2cdd47816819dcd39673cfe12a4a53df
wd <583032753@qq.com>
2014-07-29 23:37:32 +0800

    aaaaaaa

---
+++ include/abb/net/event_loop_group.hpp
@@ -0,0 +1,25 @@
+#ifndef __ABB_NET_EVENT_LOOP_GROUP_HPP__
+#define __ABB_NET_EVENT_LOOP_GROUP_HPP__
+
+#include <vector>
+#include <pthread.h>
+namespace abb{namespace net{
+class EventLoop;
+class EventLoopGroup{
+public:
+	explicit EventLoopGroup(int threads);
+	~EventLoopGroup();
+	void Start();
+	void Stop();
+	void Wait();
+	EventLoop* Next();
+private:
+	static void ThreadMain(void* arg);
+	std::vector<EventLoop*> loops_;
+	std::vector<pthread_t> threads_;
+	int num_thread_;
+	int cur_;
+};
+}}
+
+#endif
