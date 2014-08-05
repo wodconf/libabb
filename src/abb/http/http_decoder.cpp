d60889359c9d802470d4b89ab43dbfaf88c0ffd9
goalworld <583032753@qq.com>
2014-08-05 11:16:40 +0800

    x

--- src/abb/http/http_decoder.cpp
+++ src/abb/http/http_decoder.cpp
@@ -144,7 +144,6 @@ bool ResponceDecoder::Decode(base::Buffer& buf){
 			if(pos == std::string::npos){
 				continue;
 			}
-			LOG(INFO) << line.substr(0,pos) << "|"<< line.substr(pos+2);
 			rsp_->GetHeader().Set(line.substr(0,pos),line.substr(pos+2));
 		}
 
